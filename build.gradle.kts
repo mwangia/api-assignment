aaebf382f13ccbe2fb8a1c235a05fa7fb0960baf
Michael Wangia <michaelw@macbookpro.mynetworksettings.com>
2025-05-28 13:46:40 -0400

    add some observability via Spring Boot Actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     runtimeOnly("com.h2database:h2:$h2Version")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
